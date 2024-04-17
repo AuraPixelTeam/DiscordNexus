b227d8ab6413bcc84a85b90d13c806e0748240db
Harry Nguyen <nguyenhieu.c47s@gmail.com>
2024-04-17 21:08:38 +0700

    feat: init discord bot
    Run Discord Bot

--- src/DiscordNexus.js
+++ src/DiscordNexus.js
@@ -1,16 +1,35 @@
-const { Client } = require("discord.js");
-const { configDotenv } = require("dotenv");
+import { Client, GatewayIntentBits, Partials } from "discord.js";
+import { configDotenv } from "dotenv";
 
 class DiscordNexus {
 
     client;
 
     constructor() {
-        configDotenv({
-            path: ""
-        })
+        configDotenv()
+        // TODO: clean intents, partials
         this.client = new Client({
-            
+            intents: [
+                GatewayIntentBits.Guilds,
+                GatewayIntentBits.GuildMessages,
+                GatewayIntentBits.GuildMessageReactions,
+                GatewayIntentBits.GuildMembers,
+                GatewayIntentBits.GuildPresences,
+                GatewayIntentBits.GuildMessageTyping,
+                GatewayIntentBits.DirectMessages,
+                GatewayIntentBits.MessageContent,
+                GatewayIntentBits.GuildVoiceStates
+            ],
+            partials: [
+                Partials.User,
+                Partials.Channel,
+                Partials.Reaction,
+                Partials.Message,
+                Partials.GuildMember
+            ]
         })
+        this.client.login(process.env.CLIENT_TOKEN)
     }
 }
+
+new DiscordNexus()
