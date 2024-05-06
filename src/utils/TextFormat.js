baab0839d41869d04fee228c4b5bea0519048e3c
Harry Nguyen <nguyenhieu.c47s@gmail.com>
2024-05-06 07:05:40 +0700

    chore: better green color code

--- src/utils/TextFormat.js
+++ src/utils/TextFormat.js
@@ -3,7 +3,7 @@ export class TextFormat {
 
     static colors = {
         reset: "\x1b[0m",
-        green: "\x1b[32m",
+        green: "\x1b[38;5;34m",
         white: "\x1b[37m",
         red: "\x1b[31m",
         yellow: "\x1b[33m",
